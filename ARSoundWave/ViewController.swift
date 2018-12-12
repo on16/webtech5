9b6c8573024e0ee8577ba9f0a880797f6ed0ae06
Benni Asal <benedikt.asal@gmx.de>
2018-12-12 15:13:36 +0100

    task: add close button to exit camera scanning

--- ARSoundWave/ViewController.swift
+++ ARSoundWave/ViewController.swift
@@ -11,6 +11,19 @@ class ViewController: UIViewController, ARSKViewDelegate {
 
     @IBOutlet var sceneView: ARSKView!
     var selectedImage : ImageInformation?
+
+    var closeButton:UIButton = {
+        let btn = UIButton(type: .system)
+        btn.setTitle("Close", for: .normal)
+        btn.setTitleColor(.black, for: .normal)
+        btn.backgroundColor = .white
+        btn.frame = CGRect(x: 0, y: 0, width: 70, height: 40)
+        btn.center = CGPoint(x: UIScreen.main.bounds.width*0.15, y: UIScreen.main.bounds.height*0.1)
+        btn.layer.cornerRadius = btn.bounds.height/8
+        btn.alpha = 1
+        btn.isEnabled = true
+        return btn
+    }()
     
     let images = [
         "soundwave" : ImageInformation(
@@ -23,11 +36,15 @@ class ViewController: UIViewController, ARSKViewDelegate {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        
+
+        self.view.addSubview(closeButton)
+
+        closeButton.addTarget(self, action: #selector(closeAction(sender:)), for: .touchUpInside)
+
         sceneView.delegate = self
         sceneView.showsFPS = true
         sceneView.showsNodeCount = true
-        
+
         if let scene = SKScene(fileNamed: "Scene") {
             sceneView.presentScene(scene)
         }
@@ -40,7 +57,12 @@ class ViewController: UIViewController, ARSKViewDelegate {
         configuration.detectionImages = referenceImages
         sceneView.session.run(configuration, options: [.resetTracking, .removeExistingAnchors])
     }
-    
+
+
+    // Pause and resume method
+    @objc func closeAction(sender:UIButton) {
+        self.performSegue(withIdentifier: "closeCamera", sender: self)
+    }
 
     func view(_ view: ARSKView, nodeFor anchor: ARAnchor) -> SKNode? {
         if let imageAnchor = anchor as? ARImageAnchor,
