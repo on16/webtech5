aee6b5e2cebf0559b5e7a3416bcb703ca9efc74d
Benni Asal <benedikt.asal@gmx.de>
2019-01-31 10:18:51 +0100

    task: add PVC

---
+++ ARSoundWave/HelpPageViewController.swift
@@ -0,0 +1,30 @@
+//
+//  HelpPageViewController.swift
+//  ARSoundWave
+//
+//  Created by Benni  on 31.01.19.
+//  Copyright © 2019 Benni . All rights reserved.
+//
+
+import UIKit
+
+class HelpPageViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ HelppageViewController.swift
@@ -0,0 +1,55 @@
+//
+//  HelppageViewController.swift
+//  ARSoundWave
+//
+//  Created by Benni  on 31.01.19.
+//  Copyright © 2019 Benni . All rights reserved.
+//
+
+import UIKit
+
+class HelppageViewController: UIPageViewController, UIPageViewControllerDataSource {
+
+    lazy var controllerList:[UIViewController] = {
+        let storyBoard = UIStoryboard(name: "Main", bundle: nil)
+
+        let firstController = storyBoard.instantiateViewController(withIdentifier: "firstHelpView")
+        let secondController = storyBoard.instantiateViewController(withIdentifier: "secondHelpView")
+        let thirdController = storyBoard.instantiateViewController(withIdentifier: "thirdHelpView")
+
+        return [firstController, secondController, thirdController]
+    }()
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        self.dataSource = self
+
+        if let firstStep = controllerList.first {
+            self.setViewControllers([firstStep], direction: .forward, animated: false, completion: nil)
+        }
+
+    }
+
+    func pageViewController(_ pageViewController: UIPageViewController, viewControllerBefore viewController: UIViewController)
+                    -> UIViewController? {
+        guard let currentControllerIndex = controllerList.index(of: viewController) else {return nil}
+
+        let prevIndex = currentControllerIndex - 1
+        guard prevIndex >= 0 else {return nil}
+        guard controllerList.count > prevIndex else {return nil}
+
+        return controllerList[prevIndex]
+    }
+
+    func pageViewController(_ pageViewController: UIPageViewController, viewControllerAfter viewController: UIViewController)
+                    -> UIViewController? {
+        guard let currentControllerIndex = controllerList.index(of: viewController) else {return nil}
+
+        let nextIndex = currentControllerIndex + 1
+        guard controllerList.count != nextIndex else {return nil}
+        guard controllerList.count > nextIndex else {return nil}
+
+        return controllerList[nextIndex]
+    }
+}
